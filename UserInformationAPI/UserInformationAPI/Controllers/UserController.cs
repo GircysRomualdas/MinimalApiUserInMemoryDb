d13505963d758056e05c82700fb9a4e4ab859d99
Rudis159 <77154023+Rudis159@users.noreply.github.com>
2023-04-01 15:18:31 +0300

    Create controller
    created controller UserController

--- UserInformationAPI/UserInformationAPI/Controllers/UserController.cs
+++ UserInformationAPI/UserInformationAPI/Controllers/UserController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using UserInformationAPI.Models;
+using UserInformationAPI.Data;
+
 namespace UserInformationAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly ApiContext _context;
+
+        public UserController(ApiContext context)
+        {
+            _context = context;
+        }
     }
 }
